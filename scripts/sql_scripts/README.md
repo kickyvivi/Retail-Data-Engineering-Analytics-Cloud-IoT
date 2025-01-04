75d07eef431f46bc4ea831152520ce841b1f21c9
Vivek N R <vivi94458@gmail.com>
2025-01-04 14:22:36 -0500

    Adding the stg_store, dim_store and dim_inventory staging and main tables in OCI ADW

---
+++ scripts/sql_scripts/README.md
@@ -0,0 +1,4 @@
+## SQL Scripts
+- **staging_tables.sql**: Contains CREATE TABLE statements for staging tables.
+- **main_tables.sql**: Contains CREATE TABLE statements for main dimension and fact tables.
+- Use dry runs or CLI to deploy these scripts in BigQuery.
---
+++ scripts/sql_scripts/oci_adw_staging_tables.sql
@@ -0,0 +1,14 @@
+CREATE TABLE retail_iot_project.stg_store (
+    store_id NUMBER,
+    store_name VARCHAR2(10),
+    street VARCHAR2(200),
+    city VARCHAR2(100),
+    state VARCHAR2(2),
+    region VARCHAR2(50),
+    country VARCHAR2(50),
+    postal_code NUMBER,
+    store_open_date DATE,
+    store_close_date DATE,
+    insert_timestamp TIMESTAMP,
+    processed_flag CHAR(1) DEFAULT 'N'
+);
