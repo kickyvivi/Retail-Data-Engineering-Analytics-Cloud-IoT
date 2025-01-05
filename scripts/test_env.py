d5cf37cdaff0eabe01e796500d04dc1fdbb1eb52
Vivek N R <vivi94458@gmail.com>
2025-01-05 15:39:31 -0500

    chore: add environment test script

---
+++ scripts/test_env.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import os
+import argparse
+from datetime import datetime
+
+#Initialize argument parser
+parser = argparse.ArgumentParser(description="Test environment by genrating a sample dataset.")
+parser.add_argument("--output_path", type=str, required=True, help="Directory to save the output CSV file.")
+
+#Parse arguments
+args = parser.parse_args()
+output_dir = args.output_path
+
+#Ensure the output directory exists
+os.makedirs(output_dir, exist_ok=True)
+
+#Generate sample data
+data = {
+    "id": [1, 2, 3, 4, 5],
+    "name": ["Alice", "Alan", "Murphy", "Jack", "Silva"],
+    "age": [18, 20, 35, 29, 69],
+    "timestamp": [datetime.now() for _ in range(5)]
+}
+
+#Create dataframe
+df = pd.DataFrame(data)
+
+#Save to CSV File
+output_file = os.path.join(output_dir, "sample_data.csv")
+df.to_csv(output_file, index=False)
+
+print(f"Sample file saved to {output_file} in {output_dir}")
