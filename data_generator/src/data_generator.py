9a569ad33e958949769c509ce734b0588194e358
Vivek N R <vivi94458@gmail.com>
2025-01-06 17:22:57 -0500

    chore: updated directory structure and working Dockerfile

---
+++ data_generator/src/data_generator.py
@@ -0,0 +1,10 @@
+import os
+
+# Access the config file
+config_path = os.path.join("config", "config.json")
+
+# Save output files
+output_path = os.path.join("output", "customer", "customer_data.csv")
+
+# Test script
+print("Hello world")
