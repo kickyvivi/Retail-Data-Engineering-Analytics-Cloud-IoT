23dff95597378c6428988f01fa750c61376e9d3a
Vivek N R <vivi94458@gmail.com>
2025-01-25 02:14:49 -0500

    feat: implement logging for stg_customer pipeline

--- beam_pipelines/src/stg_customer.py
+++ beam_pipelines/src/stg_customer.py
@@ -1,5 +1,6 @@
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.metrics import Metrics
 import datetime as datetime
 from common.src.config_loader import load_config
 from common.src.logging_config import setup_logger
@@ -13,7 +14,12 @@ pipeline_config = config["gcs"]["stg_customer"]
 
 # Setup logger
 logger = setup_logger('logs/pipeline/stg_customer.log')
+logger.info("********Execution Started********")
 logger.info(f"Loaded configuration: {pipeline_config}")
+
+# Setup Metrics
+processed_rows = Metrics.counter('stg_customer', 'processed_rows')
+error_rows = Metrics.counter('stg_customer', 'error_rows')
 
 # Define the pipeline options
 class BeamOptions(PipelineOptions):
@@ -24,20 +30,26 @@ class BeamOptions(PipelineOptions):
 
 # Function to transform the data
 def transform_data(row):
-    fields = row.split(',')
-    return {
-        'customer_id': int(fields[0]),
-        'first_name': fields[1],
-        'last_name': fields[2],
-        'email': fields[3],
-        'age': int(fields[4]),
-        'city': fields[5],
-        'state': fields[6],
-        'country': fields[7],
-        'postal_code': fields[8],
-        'insert_timestamp': datetime.datetime.now(datetime.timezone.utc).isoformat(),
-        'processed_flag': 'FALSE'
-    }
+    try:
+        fields = row.split(',')
+        processed_rows.inc() # Increment the processed rows counter
+        return {
+            'customer_id': int(fields[0]),
+            'first_name': fields[1],
+            'last_name': fields[2],
+            'email': fields[3],
+            'age': int(fields[4]),
+            'city': fields[5],
+            'state': fields[6],
+            'country': fields[7],
+            'postal_code': fields[8],
+            'insert_timestamp': datetime.datetime.now(datetime.timezone.utc).isoformat(),
+            'processed_flag': 'FALSE'
+        }
+    except Exception as e:
+        error_rows.inc() # Increment the error rows counter
+        logger.error(f"Error processing row: {row}. Error: {e}")
+        return None
 
 # Main pipeline
 def run(argv=None):
@@ -54,25 +66,52 @@ def run(argv=None):
     options.view_as(PipelineOptions).view_as(beam.options.pipeline_options.StandardOptions).runner = runner
     options.view_as(PipelineOptions).view_as(beam.options.pipeline_options.GoogleCloudOptions).temp_location = temp_location
 
-    logger.info(f"Runner: {runner}, Temp Location: {temp_location}, Input Path: {input_path}, Output Table: {output_table}")
-
-    with beam.Pipeline(options=options) as p:
-        (
-            p
-            | "Read CSV from GCS" >> beam.io.ReadFromText(input_path, skip_header_lines=1)
-            | "Transform Data" >> beam.Map(transform_data)
-            | "Write to BigQuery" >> beam.io.WriteToBigQuery(
-                output_table,
-                schema=(
-                    'customer_id:INTEGER,first_name:STRING,last_name:STRING,email:STRING,'
-                    'age:INTEGER,city:STRING,state:STRING,country:STRING,postal_code:INTEGER,'
-                    'insert_timestamp:TIMESTAMP,processed_flag:BOOLEAN'
-                ),
-                custom_gcs_temp_location=temp_location,
-                create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
-                write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND
+    try:
+        logger.info(f"Pipeline started with Runner: {runner}, Temp Location: {temp_location}, Input Path: {input_path}, Output Table: {output_table}")
+        
+        with beam.Pipeline(options=options) as p:
+            (
+                p
+                | "Read CSV from GCS" >> beam.io.ReadFromText(input_path, skip_header_lines=1)
+                | "Transform Data" >> beam.Map(transform_data)
+                | "Write to BigQuery" >> beam.io.WriteToBigQuery(
+                    output_table,
+                    schema=(
+                        'customer_id:INTEGER,first_name:STRING,last_name:STRING,email:STRING,'
+                        'age:INTEGER,city:STRING,state:STRING,country:STRING,postal_code:INTEGER,'
+                        'insert_timestamp:TIMESTAMP,processed_flag:BOOLEAN'
+                    ),
+                    custom_gcs_temp_location=temp_location,
+                    create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
+                    write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND
+                )
             )
-        )
+        # Wait for pipeline exeucution to complete before retrieving metrics
+        logger.info("Write to BigQuery executed. Waiting for pipeline to complete.")
+        result = p.run()
+        result.wait_until_finish()
+
+        # Retrieving metrics
+        processed_metric = result.metrics().query(
+            beam.metrics.MetricsFilter().with_name("processed_rows")
+        )['counters']
+        
+        error_metric = result.metrics().query(
+            beam.metrics.MetricsFilter().with_name("error_rows")
+        )['counters']
+
+        processed_count = processed_metric[0].committed if processed_metric else 0
+        error_count = error_metric[0].committed if error_metric else 0
+        total_count = processed_count + error_count
+        
+        logger.info("Pipeline execution completed successfully.")
+        logger.info(f"Total rows: {total_count}")
+        logger.info(f"Total processed rows: {processed_count}")
+        logger.info(f"Total error rows: {error_count}")
+    except Exception as e:
+        logger.error(f"Pipeline failed with error: {e}", exc_info=True)
+        raise
+
 
 if __name__ == "__main__":
     run()
