8150ffdaff6e4d48473077d8fffab06876fb99a8
Vivek N R <vivi94458@gmail.com>
2025-01-25 19:39:28 -0500

    feat: implement gcs bucket blob  browser method

---
+++ common/src/list_files_gcs_bucket.py
@@ -0,0 +1,18 @@
+# Function to list files in GCS bucket
+def get_file_from_bucket(bucket_path, filename_template):
+    """Retrieve the first file matching the template from the bucket."""
+    # Extract bucket name and prefix
+    bucket_name = bucket_path.replace("gs://", "").split("/")[0]
+    prefix = "/".join(bucket_path.replace("gs://", "").split("/")[1:])
+    
+    # Initialize GCS client
+    client = storage.Client()
+    bucket = client.get_bucket(bucket_name)
+    blobs = bucket.list_blobs(prefix=prefix)
+    
+    # Filter files matching the filename template
+    for blob in blobs:
+        if re.match(filename_template, blob.name.split("/")[-1]):
+            return f"gs://{bucket_name}/{blob.name}"
+    
+    return None
